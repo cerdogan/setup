b00cb73f4d9ec5adcbcc85eedd6314ee850d8a6d
Can Erdogan <canerdogan.89@gmail.com>
2013-01-06 05:53:00 -0500

    Changes

---
+++ zsh/personal.zsh
@@ -0,0 +1,16 @@
+# Binaries
+alias gdb='gdb --args'
+alias make='for i in {1..25}; do echo ""; done; clear; make'
+alias oldowan='cd /home/cerdogan/Documents/Oldowan/build/'
+alias rube='cd /home/cerdogan/Documents/RubeGoldberg/'
+alias valgrind='valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes'
+
+# Aliases
+alias cl='/home/cerdogan/Documents/School/Random/OpenCL/'
+alias grep='grep -n --color=tty'
+
+# Paths
+export GTEST_DIR='/home/cerdogan/.Tools/gtest-1.6.0/include/'
+export INPUTRC='/home/cerdogan/.inputrc'
+export PATH=$PATH:'/usr/local/MATLAB/R2012a/bin'
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:'/usr/local/lib:/home/cerdogan/Documents/RubeGoldberg/Revisited/build:/opt/AMDAPP/lib/x86_64:/opt/AMDAPP/lib/x86:/usr/lib/fglrx'
