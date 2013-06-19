e1c7e7ec647ba92a5e4b871de944c65a0a01e7cc
Can Erdogan <cerdogan3@gatech.edu>
2013-06-19 07:55:43 -0400

    Added gdb pretty printing functionality for stl and eigen

--- zsh/personal.zsh
+++ zsh/personal.zsh
@@ -13,16 +13,18 @@ alias valgrind='valgrind --tool=memcheck --leak-check=yes --show-reachable=yes -
 alias cl='/home/cerdogan/Documents/School/Random/OpenCL/'
 alias grep='grep -n --color=tty'
 alias tname='tmux rename-window' 
-alias kdoc='cd /home/cerdogan/Documents/Krang/Documentation'
-alias ksoft='cd /home/cerdogan/Documents/Krang/Software'
+alias kdoc='cd /home/cerdogan/Documents/Documentation'
+alias ksoft='cd /home/cerdogan/Documents/Software'
 alias isrr='cd /home/cerdogan/Documents/MacGyver/ISRR'
 alias scenes='cd /home/cerdogan/Documents/MacGyver/3rdParty/simulation/scenes'
+alias esdcan='/usr/src/ntcan/esdcan-pci405-linux-2.6.x-x86_64-3.9.4'
+alias exp='~/Documents/Software/project/krang/experiments/'
 
 # Paths
 export GTEST_DIR='/home/cerdogan/.Tools/gtest-1.6.0/include/'
 export INPUTRC='/home/cerdogan/.inputrc'
 export PATH=$PATH:'/usr/local/MATLAB/R2012a/bin'
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:'/usr/local/lib:/home/cerdogan/Documents/RubeGoldberg/Revisited/build:/opt/AMDAPP/lib/x86_64:/opt/AMDAPP/lib/x86:/usr/lib/fglrx'
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:'/usr/lib:/usr/local/lib:/home/cerdogan/Documents/RubeGoldberg/Revisited/build:/opt/AMDAPP/lib/x86_64:/opt/AMDAPP/lib/x86:/usr/lib/fglrx'
 export MATLAB_JAVA=/usr/lib/jvm/java-7-openjdk-amd64/jre
 
 # Research
